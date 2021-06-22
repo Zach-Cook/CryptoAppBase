5e9a8180067c9c9ffe08ec05cd9e5dea447bfaea
Zach_Cook_PC <zachcook8@gmail.com>
2021-06-22 10:53:27 -0500

    added the identicon

--- web-client/src/containers/navigation.js
+++ web-client/src/containers/navigation.js
@@ -1,8 +1,8 @@
 import React from 'react'
-
 // component
 import { Navigation } from '../components';
-
+// Identicon
+import Identicon from 'react-identicons';
 
 
 export default function NavigationContainer({userState}) {
@@ -11,8 +11,17 @@ export default function NavigationContainer({userState}) {
     return (
         <Navigation>
             <Navigation.NavigationInner>
-            <Navigation.TitleText>Account: {userState ? userState.account : null}</Navigation.TitleText>
             <Navigation.TitleText>DecentralTube</Navigation.TitleText>
+            {
+                userState ?
+                <div style={{display: "flex", justifyContent: "space-between", alignItems: "center", width: "40%"}}>
+                    <Navigation.TitleText>Account: {userState.account}</Navigation.TitleText>
+                    <Identicon string={userState.account} size={40}/>
+                </div>
+                :
+                null
+            }
+            
             </Navigation.NavigationInner>
         </Navigation>
 
